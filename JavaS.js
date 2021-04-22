a4588fb72fc6fb9936953bcd2e68cacede0289ea
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-04-23 03:00:10 +0400

    change button color on click

---
+++ JavaS.js
@@ -0,0 +1,10 @@
+const button = document.querySelector(".square")
+
+let hey = document.createElement("h1")
+hey.innerHTML = 'heeeey'
+
+let b = document.querySelector("body")
+
+const changeColor = () => {
+    button.style.color = "red"
+}
