3f6145808bc1f33f0bd13afed146180cc3abf292
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-04-23 22:26:30 +0400

    make the logic of winning the XO (succeeded)

--- JavaS.js
+++ JavaS.js
@@ -21,3 +21,28 @@ const addIdToP1 = (x) => {
     }
     counter++
 }
+const win = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+]
+const checkWinner = () => {
+    win.forEach(function(elem, i) {
+        if (player1.sort().join("").includes(elem.join("")) /*player1 has one of the 3 numbers in win array*/ ) {
+            /*alert that player 1 has won (or a big picture ) */
+            /*with sound effect */
+            b.append("Player1 Has WON")
+        }
+        if (player2.sort().join("").includes(elem.join("")) /*player2 has one of the 3 numbers in win array*/ ) {
+            /*alert that player 2 has won (or a big picture ) */
+            /*with sound effect */
+            b.append("Player2 Has WON")
+        }
+    })
+
+}
