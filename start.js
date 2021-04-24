304351d3ff07c99fd7602b2076b6f9b31bba9f1a
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-04-24 23:41:25 +0400

    add sound effect and time related actions on home

---
+++ start.js
@@ -0,0 +1,31 @@
+const sure = () => {
+    document.getElementById("cont").style.visibility = 'visible'
+    document.getElementById("start").style.visibility = 'hidden'
+
+
+    setTimeout(hrV, 500)
+    playSound()
+
+}
+
+const hrV = () => {
+    document.getElementById("hr").style.visibility = 'visible'
+
+
+    setTimeout(hungryV, 2000)
+
+}
+const hungryV = () => {
+    document.getElementById("hun").style.visibility = 'visible'
+
+
+    setTimeout(thinkV, 2000)
+}
+
+const thinkV = () => {
+    document.getElementById("think").style.visibility = 'visible'
+
+}
+
+const playSound = () => bgsound.play();
+setTimeout(playSound, 1000)
